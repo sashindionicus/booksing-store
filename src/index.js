a04fb847cffb85c5e7f3272c9f8a24ae5bf77aab
sashindionicus <sashindionicus@gmail.com>
2021-10-27 10:43:20 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log('hello')
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from './components/app'
+import ErrorBoundry from './components/error-boundry';
+import BookstoreService from './services/bookstore-service'
+import { BookstoreServiceProvider } from "./components/bookstore-service-context";
+
+import store from './store'
